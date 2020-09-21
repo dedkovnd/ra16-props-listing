54ba0bdd014daa1a4995d54b349ac826480191e4
Nikita Dedkov <dedkovnd@gmail.com>
2020-09-21 22:20:36 +0300

    need finished Listing component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {Listing} from "./Listing";
+
+function App() {
+  return (
+    <div className="App">
+       <Listing />
+    </div>
+  );
+}
+
+export default App;
